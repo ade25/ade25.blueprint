c48103408940837afcb2667b8b362133e4eee20e
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-12-03 18:34:20 +0100

    Cleanup several components and cleanup dev profile

--- src/example.sitetheme/example/sitetheme/resources/js/application.js
+++ src/example.sitetheme/example/sitetheme/resources/js/application.js
@@ -1,16 +1,11 @@
 /*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
-/*global jQuery:false, document:false, window:false, location:false */
+/*global jQuery:false, document:false */
 
 (function ($) {
     $(document).ready(function () {
-        if (jQuery.browser.msie && parseInt(jQuery.browser.version, 10) < 7) {
-            // it's not realistic to think we can deal with all the bugs
-            // of IE 6 and lower. Fortunately, all this is just progressive
-            // enhancement.
-            return;
+        if (jQuery.browser.msie && parseInt(jQuery.browser.version, 10) < 7) {return;
         }
-        // Add custom javascript calls here
-        // e.g.
-        // $('#my-gallery').scrollable();
-    });
-}(jQuery));
+        // Application specific javascript code goes here
+      }
+    );
+  }(jQuery));
